50d8bf800039ebbe6eff391b984f0a7fdad0f8d0
Andrei <andrei.novikau@gmail.com>
2021-10-08 07:17:24 +0300

    modified js file

--- js/script.js
+++ js/script.js
@@ -32,13 +32,29 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+ 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его'); 
+    
+    // a не равно нулю и b не равно нулю и а не пустая строка и b не пустая строка и длинна а меньше 50
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }else {
+        console.log('error');
+        i--; //Если условие не выполнилось вернемся на одно повторение назад 
+    }                
+}
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его'),
-      c = prompt('Сколько фильмов вы уже посмотрели?', ''),
-      d = prompt('На сколько оцените его');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; 
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
